d74603de857bab56ffb088db085e9c63438123bf
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-11-05 01:45:46 +0700

    refactor: setup vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const router = createBrowserRouter(routes, { basename: '/User-Registration' });
+  const router = createBrowserRouter(routes);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <ToastContainer />
